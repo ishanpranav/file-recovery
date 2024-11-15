c59da81eca4598edacfe148f9ac14e93e9f74942
Ishan Pranav <ishan.pranav@outlook.com>
2024-11-14 21:54:05 -0500

    Test autograder 1.1, 1.2

--- src/main.c
+++ src/main.c
@@ -2,21 +2,52 @@
 // Copyright (c) 2024 Ishan Pranav
 // Licensed under the MIT license.
 
-#include <stdio.h>
+// References:
+//  - https://www.man7.org/linux/man-pages/man3/getopt.3.html
 
-static void main_print_usage(FILE* output)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void main_print_usage(char* args[])
 {
-    fprintf(output, 
-        "Usage: ./nyufile disk <options>\n"
+    printf(
+        "Usage: %s disk <options>\n"
         "  -i                     Print the file system information.\n"
         "  -l                     List the root directory.\n"
         "  -r filename [-s sha1]  Recover a contiguous file.\n"
-        "  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
+        "  -R filename -s sha1    Recover a possibly non-contiguous file.\n",
+        args[0]);
 }
 
-int main()
+int main(int count, char* args[])
 {
-    main_print_usage(stdout);
-    
-    return 0;
+    int option;
+
+    while ((option = getopt(count, args, "ilr:R:s:")) != -1)
+    {
+        switch (option)
+        {
+        case 'i':
+            break;
+
+        case 'l': break;
+        case 'r': break;
+        case 'R': break;
+        case 's': break;
+
+        default:
+            main_print_usage(args);
+            return EXIT_FAILURE;
+        }
+    }
+        
+    if (optind >= count)
+    {
+        main_print_usage(args);
+
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
