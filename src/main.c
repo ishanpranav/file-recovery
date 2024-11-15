b254397e6675fc3f2c278c2514cad0faf73c692f
Ishan Pranav <ishan.pranav@outlook.com>
2024-11-14 21:37:46 -0500

    Add main_print_usage

--- src/main.c
+++ src/main.c
@@ -2,7 +2,21 @@
 // Copyright (c) 2024 Ishan Pranav
 // Licensed under the MIT license.
 
+#include <stdio.h>
+
+static void main_print_usage(FILE* output)
+{
+    fprintf(output, 
+        "Usage: ./nyufile disk <options>\n"
+        "  -i                     Print the file system information.\n"
+        "  -l                     List the root directory.\n"
+        "  -r filename [-s sha1]  Recover a contiguous file.\n"
+        "  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
+}
+
 int main()
 {
+    main_print_usage(stdout);
+    
     return 0;
 }
