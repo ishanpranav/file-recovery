e5c5f9c4193e599d2e6a45a91c1af4d9808f67d9
Ishan Pranav <ishan.pranav@outlook.com>
2024-11-29 16:16:52 -0500

    Implement candidate find

--- src/recover_contiguous_utility.c
+++ src/recover_contiguous_utility.c
@@ -2,13 +2,47 @@
 // Copyright (c) 2024 Ishan Pranav
 // Licensed under the MIT license.
 
+#include <string.h>
+#include "fat32_attributes.h"
 #include "utility.h"
+#include "volume_root_iterator.h"
 
 void recover_contiguous_utility(
     FILE* output,
     Volume volume,
-    UTILITY_UNUSED char* recover,
+    char* recover,
     UTILITY_UNUSED char* sha1)
 {
+    bool candidate = false;
+    struct VolumeRootIterator it;
 
+    for (volume_root_begin(&it, volume); !it.end; volume_root_next(&it))
+    {
+        if (it.entry->attributes & FAT32_ATTRIBUTES_DIRECTORY ||
+            !(fat32_directory_entry_is_end_free(it.entry) &&
+                fat32_directory_entry_is_mid_free(it.entry)))
+        {
+            continue;
+        }
+
+        char buffer[13];
+
+        volume_get_display_name(buffer, it.entry->name);
+
+        if (strcmp((char*)(it.entry->name + 1), recover + 1) != 0)
+        {
+            continue;
+        }
+
+        if (candidate)
+        {
+            fprintf(output, "%s: multiple candidates found\n", recover);
+        
+            return;
+        }
+
+        candidate = true;
+
+        break;
+    }
 }
